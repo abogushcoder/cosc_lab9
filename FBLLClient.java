2328980064324e27c3a827b8d9253c020adb1350
Alex <aeb0975@gmail.com>
2024-11-16 20:01:12 -0500

    finished project

--- FBLLClient.java
+++ FBLLClient.java
@@ -54,6 +54,83 @@ public class FBLLClient {
 
   }
 
+  private static void testRetainUpToEdgeCases() {
+    System.out.println("*****    RETAIN UP TO EDGE CASES    *****");
+
+    FBLinkedList<String> test1 = new FBLinkedList<String>();
+    String target = "cat";
+    System.out.println("Test #1 (empty list):");
+    System.out.println("list before:\t" + test1 + ", target: " + target);
+    test1.retainUpTo(target);
+    System.out.println("list after:\t" + test1 + "\n");
+
+    FBLinkedList<String> test2 = new FBLinkedList<String>();
+    test2.add("cat");
+    target = "cat";
+    System.out.println("Test #2 (single element, target exists):");
+    System.out.println("list before:\t" + test2 + ", target: " + target);
+    test2.retainUpTo(target);
+    System.out.println("list after:\t" + test2 + "\n");
+
+    FBLinkedList<String> test3 = new FBLinkedList<String>();
+    test3.add("cat");
+    target = "dog";
+    System.out.println("Test #3 (single element, target does not exist):");
+    System.out.println("list before:\t" + test3 + ", target: " + target);
+    test3.retainUpTo(target);
+    System.out.println("list after:\t" + test3 + "\n");
+
+    FBLinkedList<String> test4 = new FBLinkedList<String>();
+    test4.add("cat");
+    test4.add("bat");
+    test4.add("rat");
+    target = "dog";
+    System.out.println("Test #4 (target does not exist):");
+    System.out.println("list before:\t" + test4 + ", target: " + target);
+    test4.retainUpTo(target);
+    System.out.println("list after:\t" + test4 + "\n");
+
+    FBLinkedList<String> test5 = new FBLinkedList<String>();
+    test5.add("cat");
+    test5.add("bat");
+    test5.add("rat");
+    target = "cat";
+    System.out.println("Test #5 (target is first element):");
+    System.out.println("list before:\t" + test5 + ", target: " + target);
+    test5.retainUpTo(target);
+    System.out.println("list after:\t" + test5 + "\n");
+
+  }
+
+  private static void testAddEdgeCases() {
+    System.out.println("*****    ADD EDGE CASES    *****");
+
+    FBLinkedList<String> test1 = new FBLinkedList<String>();
+    System.out.println("Test #1 (add to empty list):");
+    test1.add("cat");
+    System.out.println("list after:\t" + test1 + "\n");
+
+    FBLinkedList<String> test2 = new FBLinkedList<String>();
+    System.out.println("Test #2 (add at index 0):");
+    test2.add(0, "dog");
+    System.out.println("list after:\t" + test2 + "\n");
+
+    FBLinkedList<String> test3 = new FBLinkedList<String>();
+    test3.add("cat");
+    test3.add("dog");
+    System.out.println("Test #3 (add at end of list):");
+    test3.add(2, "frog");
+    System.out.println("list after:\t" + test3 + "\n");
+
+    FBLinkedList<String> test4 = new FBLinkedList<String>();
+    test4.add("cat");
+    test4.add("dog");
+    test4.add("frog");
+    System.out.println("Test #4 (add to middle of list):");
+    test4.add(1, "bat");
+    System.out.println("list after:\t" + test4 + "\n");
+  }
+
   public static void main(String[] args) {
 
     // Come up with test cases of your own
@@ -61,6 +138,8 @@ public class FBLLClient {
 
     testAdd(); // uncomment me for Part 2.3!
     testRetainUpTo(); // uncomment me for Part 2.6!
+    testAddEdgeCases();
+    testRetainUpToEdgeCases();
 
   }
 
